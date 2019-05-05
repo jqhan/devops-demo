3ad21ea534d96306dcfc793b92282b9219b92de3
Rosquist Oscar <oscarros@kth.com>
2019-05-05 19:54:08 +0200

    change hook script

--- generate-html.sh
+++ generate-html.sh
@@ -33,7 +33,3 @@ str=$str"
 echo $str > index.html
 aws s3 cp index.html s3://demo-reports-devops-demo/index.html --acl public-read > /dev/null
 rm index.html
-echo "--------------------------------------"
-echo "Reports uploaded."
-echo "url to reports: http://demo-reports-devops-demo.s3-website-eu-west-1.amazonaws.com/"
-echo "--------------------------------------"
